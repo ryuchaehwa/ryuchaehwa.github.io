be334e3a343cbbf827983b9200134b95f10fd7aa
ryuchaehwa <r.chaehwa@gmail.com>
2025-08-26 14:42:32 +0900

    .

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "https://ryuchaehwa.github.io/",
+  // publicPath: "https://ryuchaehwa.github.io/",
   outputDir: "./doc",
 });
