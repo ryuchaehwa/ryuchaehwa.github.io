90f47bf8cd759a4f3a333270cfa5a9e091b233b0
ryuchaehwa <r.chaehwa@gmail.com>
2025-08-26 14:15:38 +0900

    init

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'https://ryuchaehwa.github.io/',
-  outputDir: '/doc'
-})
+  publicPath: "https://ryuchaehwa.github.io/",
+  outputDir: "./doc",
+});
